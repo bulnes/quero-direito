bfdab394b7597f67cb7e14df8420a871f83b9474
Bruno Bulnes <bulnes.bruno@gmail.com>
2025-09-08 23:24:49 -0300

    feat: create utils to create whatsapp link

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,12 @@
-import { clsx, type ClassValue } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { DEFAULT_WHATSAPP_MESSAGE, DEFAULT_WHATSAPP_PHONE } from "@/constants";
+import { clsx, type ClassValue } from "clsx";
+import { twMerge } from "tailwind-merge";
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
+}
+
+export function getWhatsappLink(message = DEFAULT_WHATSAPP_MESSAGE, phone = DEFAULT_WHATSAPP_PHONE) {
+  const encodedMessage = encodeURIComponent(message);
+  return `https://wa.me/${phone}?text=${encodedMessage}`;
 }
